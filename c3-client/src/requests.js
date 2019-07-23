58afef5aee3a98bbf001cb1193799269ea4b8482
Wes Coderre <wesley.coderre@gmail.com>
2019-07-22 17:34:20 -0700

    Fetching users from serverless function

--- c3-client/src/requests.js
+++ c3-client/src/requests.js
@@ -1,10 +1,8 @@
-const BASE_URL = 'http://localhost:5000/api';
-
 export const Github = {
   async getMembers() {
     try {
-      const response = await fetch(`${BASE_URL}/github/members`, {
-        credentials: 'include'
+      const response = await fetch(`https://9ek0by3c34.execute-api.us-east-1.amazonaws.com/dev/hello`, {
+        //credentials: 'include' // This is not necessary for serverless in a simple requests i.e. GET
       });
       const json = await response.json();
       return json;
