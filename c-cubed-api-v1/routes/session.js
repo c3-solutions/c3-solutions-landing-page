2230df1b43a98cab0a22374c639ffb15d4f00bc5
Wes Coderre <wesley.coderre@gmail.com>
2019-07-24 18:51:36 -0700

    Authorization for request

--- c-cubed-api-v1/routes/session.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require('../controllers/session.js')
-
-router.get("/session/callback", controller.get)
-
-module.exports = router;
